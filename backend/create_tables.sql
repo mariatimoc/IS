2a5c5c99605d32286bbe0da3ecd5388c5e2d76e0
Delia Vornicu <Vornicu.Ne.Delia@student.utcluj.ro>
2025-11-20 22:41:45 +0200

    Backend cu tabele, conexiune-db si endpoint-uri

--- backend/create_tables.sql
+++ backend/create_tables.sql
@@ -25,3 +25,22 @@ CREATE TABLE alerts (
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (patient_id) REFERENCES patients(id)
 );
+
+INSERT INTO patients (name, age, code, salon) VALUES
+("Ana", 25, "Pacient001", "Salon 3"),
+("Mihai", 60, "Pacient002", "Salon 9"),
+("Radu", 47, "Pacient003", "Salon 1"),
+("Catalina", 32, "Pacient004", "Salon 12");
+
+INSERT INTO readings (patient_id, heart_rate, temperature) VALUES
+(1, 85, 36.7),
+(1, 90, 37.1),
+(2, 70, 36.5),
+(2, 100, 38.2),
+(3, 75, 36.8);
+
+INSERT INTO alerts (patient_id, message) VALUES
+(1, "Temperatura ridicata"),
+(2, "Puls anormal"),
+(3, "Hipotermie"),
+(2, "Puls foarte ridicat");
