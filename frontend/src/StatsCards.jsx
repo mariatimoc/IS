280c81637e22383859da24d9e2c9d2bbbf731932
unknown <Timoc.Ez.Maria@student.utcluj.ro>
2025-11-21 18:14:28 +0200

    frontend

---
+++ frontend/src/StatsCards.jsx
@@ -0,0 +1,33 @@
+//const values=[36.5,37.2,38.1,36.9];
+import {useEffect,useState} from "react";
+
+export default function StateCards(){
+
+
+const[stats,setStats]=useState(null);
+
+    useEffect(() =>{
+        fetch("http://127.0.0.1:8000/stats/1")
+        .then(res=>res.json())
+        .then(data=>setStats(data))
+        .catch(err=>console.log("Eroare: " ,err));
+    },[]);
+
+    if(stats==null){
+        return <p> Se incarca statisticile</p>;
+    }
+    
+
+
+    return(
+        <section>
+            <h2>Statistici </h2>
+
+            <ul>
+                <li><strong>Temperatura minima:</strong> {min} C</li>
+                <li>Temperatura maxima: {max} C</li>
+                <li><strong>Temperatura medie:</strong> {average} C </li>
+            </ul>
+        </section>
+    )
+}
