cdcab2a657068d54f7340d3b0ec7b9ca94b34258
Delia Vornicu <Vornicu.Ne.Delia@student.utcluj.ro>
2025-11-26 16:03:23 +0200

    Modificari backend

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,7 @@ def get_connection():
     connection = pymysql.connect (
         host="localhost",
         user="root",
-        password="Delia.mysql18",
+        password="",
         database="ehealth_db",
         cursorclass=pymysql.cursors.DictCursor
     )
