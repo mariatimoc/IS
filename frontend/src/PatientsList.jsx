e920f327fc99bb93fa2a6a589cb9cf5fe94310c8
unknown <Timoc.Ez.Maria@student.utcluj.ro>
2026-01-15 18:36:16 +0200

    Frontend: select pacient + add vitals + refresh

--- frontend/src/PatientsList.jsx
+++ frontend/src/PatientsList.jsx
@@ -1,17 +1,19 @@
-//const patients=[{id:1,name:"ion",salon: "101"}];
 import {useEffect,useState} from "react";
 
 
-export default function PatientsList(){
+export default function PatientsList({selectedPatientId,onSelectPatient}){
 
     const [patients,setPatients]=useState([]); //useState e folosit pt datele care se schimba
+    const [loading,setLoading]=useState(true);
     
     //useEffect se executa automata cand se incarca pagina,ia pacientii din backend
     useEffect(() =>{
-        fetch("http://127.0.0.1:8000/patients") //cere lista pacienti
-        .then(res => res.json()) //converteste in format json
-        .then(data=> setPatients(data)) 
-        .catch(err=>console.log("Eroare:",err));
+        setLoading(true);
+        fetch("http://127.0.0.1:8000/patients")
+        .then(res => res.json())
+        .then(data=> setPatients(data))
+        .catch(err=>console.log("Eroare pacienti:",err))
+        .finally(()=>setLoading(false));
 
 
     },[]); //[] inseamna sa se repete o singura data cand pag se incarca 
@@ -19,16 +21,30 @@ export default function PatientsList(){
     return (
         <section>
             <h2>Lista Pacientilor</h2>
-            
-            <ul> {/*urmeaza o lista*/}
-            {
-                patients.map((p) =>(
-                    <li key={p.id}>
-                        {p.name} -Salon {p.salon}
-                    </li>
-                ))
-            }
-        </ul>
-    </section>
+
+            {loading ? (
+                <p>Se incarca...</p>
+            ):(
+             <ul>
+                 {patients.map((p) => (
+                     <li
+                        key={p.id}
+                        onClick={() => onSelectPatient(p.id)}
+                        style={{
+                            cursor: "pointer",
+                            fontWeight: p.id === selectedPatientId ? "bold" : "normal",
+                            textDecoration: p.id === selectedPatientId ? "underline" : "none",
+                            marginBottom: 6,
+                        }}
+                     >{p.name} (Varsta: {p.age}) -{p.code} -{p.salon}
+                     </li>
+                 ))}
+             </ul>
+            )}
+
+            {selectedPatientId ==null && (
+                <p style={{opacity :0.7}}> Selecteaza un pacient din lista</p>
+            )}
+        </section>
     );
 }
